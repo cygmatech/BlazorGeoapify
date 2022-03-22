e4c73ae0db519b0ef5a1af28bb53b825935a11aa
cygmatech <cygmatech@gmail.com>
2022-03-21 23:41:34 -0300

    Add project files.

---
+++ BlazorGeoapify/npm_package/src/index.js
@@ -0,0 +1,14 @@
+﻿import { GeocoderAutocomplete } from '@geoapify/geocoder-autocomplete';
+
+const autocomplete = new GeocoderAutocomplete(
+    document.getElementById("autocomplete"),
+    'YOUR_API_KEY',
+    { placeholder: "Digite aqui um endereço" });
+
+autocomplete.on('select', (location) => {
+
+});
+
+autocomplete.on('suggestions', (suggestions) => {
+
+});
